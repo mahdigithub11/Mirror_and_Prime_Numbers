af9469f7dba7df1aa131d61607626c6aa9a19a35
asus <asus@LAPTOP-HV0IU0EP>
2023-10-07 08:52:04 -0700

    Add project files.

---
+++ Mirror_Number/Operation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirror_Number
+{
+    internal class Operation
+    {
+        public bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool IsPalindrome(int number)
+        {
+            int reversedNumber = 0;
+            int originalNumber = number;
+
+            while (number > 0)
+            {
+                int remainder = number % 10;
+                reversedNumber = (reversedNumber * 10) + remainder;
+                number = number / 10;
+            }
+
+            return originalNumber == reversedNumber;
+        }
+    }
+}
---
+++ Mirror_Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Mirror_Number;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number:");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        Operation operation = new Operation();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Prime numbers between {0} and {1}:", firstNumber, secondNumber);
+        for (int i = firstNumber; i <= secondNumber; i++)
+        {
+            if (operation.IsPrime(i))
+                Console.Write(i + "-");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+
+        Console.WriteLine("Palindrome numbers between {0} and {1}:", firstNumber, secondNumber);
+        for (int i = firstNumber; i <= secondNumber; i++)
+        {
+            if (operation.IsPalindrome(i))
+                Console.Write(i + "-");
+        }
+
+        Console.ReadLine();
+    }
+}
